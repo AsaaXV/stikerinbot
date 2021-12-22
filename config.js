c47503063a6375f894f52471b87a7ac5e8d5e503
AsaaXV <89570595+AsaaXV@users.noreply.github.com>
2021-12-22 15:11:15 +0800

    Update stiker.js

--- config.js
+++ config.js
@@ -33,11 +33,15 @@ global.APIKeys = { // APIKey nya disini
 global.packname = 'INI STIKER'
 global.author = 'Saya bukan Dia'
 
+// Stiker no wm 
+global.packname1 = '\n'
+global.author1 = '\n'
+
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
 
-global.multiplier = 100 // Semakin tinggi, semakin sulit naik level
+global.multiplier = 10000 // Semakin tinggi, semakin sulit naik level
 
 let chalk = require('chalk')
 let file = require.resolve(__filename)
