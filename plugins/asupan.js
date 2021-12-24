a7b4176af9a1017f29321df6c4835d831416e05a
AsaaXV <89570595+AsaaXV@users.noreply.github.com>
2021-12-24 14:09:59 +0800

    Update

--- plugins/asupan.js
+++ plugins/asupan.js
@@ -1,8 +1,10 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
-  conn.sendFile(m.chat, API('anu', `/asupan/v2/${pickRandom(['chika','delvira','ayu','bunga','aura','nisa','ziva','yana','viona','syania','riri','syifa','mama_gina','alcakenya','geayubi','santy','ukhty'])}`, {}, 'apikey'), 'asupan.mp4', '© botbang')
+  conn.sendFile(m.chat, API('anu', `/asupan/v2/${pickRandom(asupan)}`, {}, 'apikey'), 'asupan.mp4', '© botbang')
 }
 handler.help = ['asupan']
 handler.tags = ['fun']
 handler.command = /^(asupan)$/i
 
 module.exports = handler
+
+global.asupan = ["chika","delvira","ayu","bunga","aura","nisa","ziva","yana","viona","syania","riri","syifa","mama_gina","alcakenya","geayubi","santy","ukhty"]
