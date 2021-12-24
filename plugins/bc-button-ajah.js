adb643ccd5b8856a61483597113c6dadb1d89737
AsaaXV <89570595+AsaaXV@users.noreply.github.com>
2021-12-24 11:51:49 +0800

    Update

--- plugins/bc-button-ajah.js
+++ plugins/bc-button-ajah.js
@@ -9,12 +9,12 @@ let handler  = async (m, { conn, text }) => {
   let thumb = 'https://telegra.ph/file/92b9ea9d9a08be254ff07.jpg'
   let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
   let content = await conn.cMod(m.chat, m, /bc|broadcast/i.test(text) ? text : text )
-  for (let id of chats) /*conn.send2ButtonLoc*/conn.sendButtonLoc(id, await (await fetch(thumb)).buffer(), `${text}`.trim(), `all brotkes || Made whit *_@_andy.offc_*`, 'Owner', '.qw', /*'Donasi', '.ds'*/)
+  for (let id of chats) /*conn.send2ButtonLoc*/conn.sendButton(id, `${text}`.trim(), `all brotkes || Made whit *_@_andy.offc_*`, 'Owner', '.qw', /*'Donasi', '.ds'*/)
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
 }
-handler.help = ['broadcastl','bcl'].map(v => v + ' <teks>')
+handler.help = ['broadcast2','bc2'].map(v => v + ' <teks>')
 handler.tags = ['owner']
-handler.command = /^(broadcastl|bcl)$/i
+handler.command = /^(broadcast2|bc2)$/i
 handler.owner = true
 handler.mods = false
 handler.premium = false
@@ -32,5 +32,5 @@ const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
 //
-// B U A T - B U T T O N - L O C
+// B U A T - B U T T O N - A J A H
 //
--- plugins/bc-button-img.js
+++ plugins/bc-button-img.js
@@ -6,15 +6,15 @@
 }*/
 let fetch = require('node-fetch')
 let handler  = async (m, { conn, text }) => {
-  let thumb = 'https://telegra.ph/file/92b9ea9d9a08be254ff07.jpg'
+  let ituh = 'https://telegra.ph/file/92b9ea9d9a08be254ff07.jpg' //foto muh
   let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
   let content = await conn.cMod(m.chat, m, /bc|broadcast/i.test(text) ? text : text )
-  for (let id of chats) /*conn.send2ButtonLoc*/conn.sendButtonLoc(id, await (await fetch(thumb)).buffer(), `${text}`.trim(), `all brotkes || Made whit *_@_andy.offc_*`, 'Owner', '.qw', /*'Donasi', '.ds'*/)
+  for (let id of chats) /*conn.send2ButtonLoc*/conn.sendButtonImg(id, `${text}`.trim(), 'https://telegra.ph/file/92b9ea9d9a08be254ff07.jpg', `all brotkes || Made whit *_@_andy.offc_*`, 'Owner', '.qw', /*'Donasi', '.ds'*/)
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
 }
-handler.help = ['broadcastl','bcl'].map(v => v + ' <teks>')
+handler.help = ['broadcast3','bc3'].map(v => v + ' <teks>')
 handler.tags = ['owner']
-handler.command = /^(broadcastl|bcl)$/i
+handler.command = /^(broadcast3|bc3)$/i
 handler.owner = true
 handler.mods = false
 handler.premium = false
@@ -32,5 +32,5 @@ const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
 //
-// B U A T - B U T T O N - L O C
+// B U A T - B U T T O N - I M A G E
 //
