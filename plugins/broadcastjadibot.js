8d63a4764fceb871bf8048fb58394b7fe91646ca
AsaaXV <89570595+AsaaXV@users.noreply.github.com>
2021-12-25 20:40:51 +0800

    Anu

--- plugins/broadcastjadibot.js
+++ plugins/broadcastjadibot.js
@@ -1,13 +1,18 @@
-let handler = async (m, { conn, text }) => {
-  if (conn.user.jid === global.conn.user.jid) {
-    let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user.jid)])]
-    let content = await conn.cMod(m.chat, m, /bc|broadcast/i.test(text) ? text : text + '\n' + readMore + '「 All Jadibot Broadcast 」')
-    for (let id of users) conn.copyNForward(id, content)
-    conn.reply(m.chat, `_Berhasil mengirim broadcast ke ${users.length} nomor yang jadi bot_
-${users.map(v => 'wa.me/' + v.replace(/[^0-9]/g,'') + '?text=.menu').join('\n')}`.trim(), m)
-  } else conn.reply(m.chat, 'Fitur ini hanya untuk host bot',  m)
+let handler = async (m, { conn, usedPrefix, text }) => {
+  if (conn.user.jid !== global.conn.user.jid) throw false
+  let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user.jid)])]
+  let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
+  let teks = text ? text : cc.text
+  let content = conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '〔 Stikerin Broadcast 〕\n\n' + teks)
+  for (let id of users) {
+    await delay(1500)
+    await conn.copyNForward(id, content, true)
+  }
+  conn.reply(m.chat, `_Berhasil mengirim broadcast ke ${users.length} nomor yang jadi bot_
+${users.map(v => 'wa.me/' + v.replace(/[^0-9]/g, '') + `?text=${encodeURIComponent(usedPrefix)}menu`).join('\n')}
+\nestimasi selesai ${users.length * 1.5} detik`.trim(), m)
 }
-handler.help = ['broadcastjadibot','bcbot'].map(v => v + ' <teks>')
+handler.help = ['broadcastjadibot', 'bcbot'].map(v => v + ' <teks>')
 handler.tags = ['host']
 handler.command = /^(broadcast|bc)(jadi)?bot$/i
 handler.owner = true
@@ -26,3 +31,4 @@ module.exports = handler
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
+const delay = time => new Promise(res => setTimeout(res, time))
